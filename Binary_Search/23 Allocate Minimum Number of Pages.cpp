743d5a701851f14959a2846009fbf170bafd222c
Priyansh Jain <priyansh01jain@gmail.com>
2025-04-04 22:00:57 +0530

    updated

--- Binary_Search/23 Allocate Minimum Number of Pages.cpp
+++ Binary_Search/23 Allocate Minimum Number of Pages.cpp
@@ -1,7 +1,7 @@
 // NOTE Problem Statement: Given an array ‘arr of integer numbers, ‘ar[i]’ represents the number of pages in the ‘i-th’ book. There are a ‘m’ number of students, and the task is to allocate all the books to the students.
 // Allocate books in such a way that :
 
-//Each student gets at least one book.Each book should be allocated to only one student.Book allocation should be in a contiguous manner.You have to allocate the book to ‘m’ students such that the maximum number of pages assigned to a student is minimum.If the allocation of books is not possible.return -1
+// Each student gets at least one book.Each book should be allocated to only one student.Book allocation should be in a contiguous manner.You have to allocate the book to ‘m’ students such that the maximum number of pages assigned to a student is minimum.If the allocation of books is not possible.return -1
 
 // REVIEW - Time Complexity:  O(N * log(sum(arr[])-max(arr[])+1))
 
@@ -9,7 +9,7 @@
 
 #include <iostream>
 #include <vector>
-#include <numeric>   //REVIEW -
+#include <numeric>   //REVIEW - for accumulate
 #include <algorithm> //REVIEW -
 using namespace std;
 
@@ -56,7 +56,7 @@ int findPages(vector<int> &arr, int n, int m)
             low = mid + 1;
         }
     }
-    return low; // REVIEW - 
+    return low; // REVIEW -
 }
 
 int main()
