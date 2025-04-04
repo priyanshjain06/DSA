0d8ead7bc294e5bb3da0eaed25d3d9c70cbf7091
Priyansh Jain <priyansh01jain@gmail.com>
2025-04-04 22:52:08 +0530

    update !

--- Binary_Search/24 Split Array -Minimum of max sum of elements of subarrays.cpp
+++ Binary_Search/24 Split Array -Minimum of max sum of elements of subarrays.cpp
@@ -1,5 +1,3 @@
-// NOTE Problem Statement: Given an integer array ‘A’ of size ‘N’ and an integer ‘K'. Split the array ‘A’ into ‘K’ non-empty subarrays such that the largest sum of any subarray is minimized. Your task is to return the minimized largest sum of the split.
-//  A subarray is a contiguous part of the array.
 
 // REVIEW - Time Complexity: O(N * log(sum(arr[])-max(arr[])+1))
 
