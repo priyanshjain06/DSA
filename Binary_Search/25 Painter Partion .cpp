45bd1f1c96882edcf13f7fbe2cd99cb4d7fa6e3f
Priyansh Jain <priyansh01jain@gmail.com>
2025-04-04 23:11:06 +0530

    updated

--- Binary_Search/25 Painter Partion .cpp
+++ Binary_Search/25 Painter Partion .cpp
@@ -1,5 +1,3 @@
-// NOTE Problem Statement: Given an array/list of length ‘N’, where the array/list represents the boards and each element of the given array/list represents the length of each board. Some ‘K’ numbers of painters are available to paint these boards. Consider that each unit of a board takes 1 unit of time to paint. You are supposed to return the area of the minimum time to get this job done of painting all the ‘N’ boards under the constraint that any painter will only paint the continuous sections of boards.
-
 // SECTION same as 24
 
 #include <iostream>
