6c3fa34ab6b7cc0a5d5a6bc709755feb92b65ec5
Priyansh Jain <priyansh01jain@gmail.com>
2025-04-05 20:53:56 +0530

    update !

--- Binary_Search/28 Find the index of the row with maximum number of 1's in Binary Matrix .cpp
+++ Binary_Search/28 Find the index of the row with maximum number of 1's in Binary Matrix .cpp
@@ -1,6 +1,6 @@
 // NOTE Problem Statement: You have been given a non-empty grid ‘mat’ with 'n' rows and 'm' columns consisting of only 0s and 1s. All the rows are sorted in ascending order.
 //  Your task is to find the index of the row with the maximum number of ones.
-//SECTION  Note: If two rows have the same number of ones, consider the one with a smaller index. If there's no row with at least 1 zero, return -1.
+// SECTION  Note: If two rows have the same number of ones, consider the one with a smaller index. If there's no row with at least 1 zero, return -1.
 
 // REVIEW - Time Complexity: O(n * log(m)) and Space Complexity : O(1)
 
@@ -9,7 +9,7 @@
 using namespace std;
 int lowerBound(vector<int> arr, int n, int x)
 {
-    int low = 0, high = n - 1; //REVIEW -  high = n , n= row
+    int low = 0, high = n - 1; // REVIEW -  high = n , n= row
     int ans = n;
 
     while (low <= high)
@@ -35,7 +35,7 @@ int rowWithMax1s(vector<vector<int>> &matrix, int n, int m)
     int index = -1;
 
     // traverse the rows:
-    for (int i = 0; i < n; i++) 
+    for (int i = 0; i < n; i++)
     {
         // get the number of 1's:
         int cnt_ones = m - lowerBound(matrix[i], m, 1); // REVIEW - m=>col
