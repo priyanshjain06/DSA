a1f8d39442001ea90e34751043e760942ac49f3c
Priyansh Jain <priyansh01jain@gmail.com>
2025-04-12 11:26:56 +0530

    added

--- Strings/10 Remove the occcurence substring which is given from string.cpp
+++ Strings/10 Remove the occcurence substring which is given from string.cpp
@@ -2,3 +2,24 @@
 //  string : daabcabc => daabc=> da
 // NOTE Given two strings s and part, perform the following operation on s until all occurrences of the substring part are removed:
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+string removeOccurrences(string s, string part)
+{
+    string result;
+    int partLen = part.size();
+
+    for (char c : s)
+    {
+        result.push_back(c);
+
+        if (result.size() >= partLen && result.substr(result.size() - partLen) == part)
+        {
+            result.erase(result.end() - partLen, result.end());
+        }
+    }
+
+    return result;
+}
