aa14f70494b49d3fbd8a95219e89bbf7bdc07423
Priyansh Jain <priyansh01jain@gmail.com>
2025-04-12 09:29:45 +0530

    updated !

--- Strings/1 Reading Entire line with spaces and tabs .cpp
+++ Strings/1 Reading Entire line with spaces and tabs .cpp
@@ -10,11 +10,7 @@ int main()
     // REVIEW
     getline(cin, input); // reads the entire line, including spaces and tabs
 
-    // REVIEW to read multiple lines with spaces and tabs !
-    while (getline(cin, input))
-    {
-        input = input + '\n';
-    }
+   
     cout << "You entered: " << input << endl;
 
     return 0;
