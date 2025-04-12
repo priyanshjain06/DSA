b1731bd9b7d13efc46675af199f73578e61d4614
Priyansh Jain <priyansh01jain@gmail.com>
2025-04-12 09:45:08 +0530

    created !

--- Strings/9 Remove space from string and add.cpp
+++ Strings/9 Remove space from string and add.cpp
@@ -5,21 +5,15 @@
 #include <string>
 using namespace std;
 
-string replaceSpaces(string &str)
-{
-    string temp = "";
-    for (int i = 0; i < str.length(); i++)
-    {
-        if (str[i] == ' ')
-        {
-            temp.push_back('@');
-            temp.push_back('4');
-            temp.push_back('0');
-        }
-        else
-        {
-            temp.push_back(str[i]);
-        }
-    }
-    return temp;
-}
+class Solution {
+    public:
+      string modify(string& s) {
+          string result = "";
+          for (char ch : s) {
+              if (ch != ' ') {
+                  result.push_back(ch);
+              }
+          }
+          return result;
+      }
+  };
