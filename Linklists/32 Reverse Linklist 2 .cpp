d7571e03df2482783f534ec54955ccb229f3a524
Priyansh Jain <priyansh01jain@gmail.com>
2025-06-03 21:14:46 +0530

    updated!

---
+++ Linklists/32 Reverse Linklist 2 .cpp
@@ -0,0 +1,44 @@
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *reverseBetween(ListNode *head, int left, int right)
+    {
+        if (!head || left == right) // REVIEW
+            return head;
+
+        // Dummy node to simplify edge cases
+        ListNode *dummy = new ListNode(0);
+        dummy->next = head; // REVIEW
+
+        // Step 1: Reach the node before `left`
+        ListNode *prev = dummy;
+        for (int i = 1; i < left; ++i) // REVIEW from 1
+        {
+            prev = prev->next;
+        }
+
+        // Step 2: Start reversing from `left` to `right`
+        ListNode *curr = prev->next;
+        ListNode *next = nullptr;
+        for (int i = 0; i < right - left; ++i)
+        {
+            next = curr->next;       // next = 3
+            curr->next = next->next; // curr->next = 4
+            next->next = prev->next; // next->next = 2
+            prev->next = next;       // prev->next = 3
+        }
+
+        return dummy->next;
+    }
+};
