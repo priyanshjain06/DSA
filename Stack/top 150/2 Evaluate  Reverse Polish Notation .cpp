4da52dc4af6ef24405efd72c7de608263a9a6a58
Priyansh Jain <priyansh01jain@gmail.com>
2025-06-07 23:04:40 +0530

    Updated !

--- Stack/top 150/2 Evaluate  Reverse Polish Notation .cpp
+++ Stack/top 150/2 Evaluate  Reverse Polish Notation .cpp
@@ -1,23 +1,35 @@
-class Solution {
+class Solution
+{
 public:
-    int evalRPN(vector<string>& tokens) {
+    int evalRPN(vector<string> &tokens)
+    {
         stack<int> st;
 
-        for (const string& token : tokens) {
-            if (token == "+" || token == "-" || token == "*" || token == "/") {
-                //REVIEW first b then a 
-                int b = st.top(); st.pop(); 
-                int a = st.top(); st.pop();
+        for (const string &token : tokens) // REVIEW
+        {
+            if (token == "+" || token == "-" || token == "*" || token == "/")
+            {
+                // REVIEW first b then a
+                int b = st.top();
+                st.pop();
+                int a = st.top();
+                st.pop();
 
-                if (token == "+") st.push(a + b);
-                else if (token == "-") st.push(a - b);
-                else if (token == "*") st.push(a * b);
-                else st.push(a / b);  //REVIEW Integer division, truncate toward zero
-            } else {
-                st.push(stoi(token));  //FIXME  // REVIEW  Convert string to integer
+                if (token == "+")
+                    st.push(a + b);
+                else if (token == "-")
+                    st.push(a - b);
+                else if (token == "*")
+                    st.push(a * b);
+                else
+                    st.push(a / b); // REVIEW Integer division, truncate toward zero
+            }
+            else
+            {
+                st.push(stoi(token)); // FIXME  // REVIEW  Convert string to integer
             }
         }
 
-        return st.top();  // Final result
+        return st.top(); // REVIEW  Final result
     }
 };
