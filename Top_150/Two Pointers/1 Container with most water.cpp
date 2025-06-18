836118aea4b598ec514fe9dec9156cc522cabfbc
Priyansh Jain <priyansh01jain@gmail.com>
2025-06-18 08:53:52 +0530

    UPdated !

---
+++ Top_150/Two Pointers/1 Container with most water.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i = 0, j = height.size() - 1;
+        int maxArea = 0;
+
+        while (i < j) {
+            int h = min(height[i], height[j]);
+            int w = j - i;
+            maxArea = max(maxArea, h * w);
+
+            if (height[i] < height[j]) //REVIEW 
+                i++;
+            else
+                j--;
+        }
+
+        return maxArea;
+    }
+};
