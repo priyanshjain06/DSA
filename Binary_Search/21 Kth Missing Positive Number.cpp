3de1da5ce9bb0546f72df8fea3cc8d1444b67d35
Priyansh Jain <priyansh01jain@gmail.com>
2025-07-02 12:03:40 +0530

    Updated !

--- Binary_Search/21 Kth Missing Positive Number.cpp
+++ Binary_Search/21 Kth Missing Positive Number.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int missingK(vector<int> vec, int n, int k)
 {
     // NOTE see its burte force !
-    int low = 0, high = n - 1; //REVIEW high is n-1 
+    int low = 0, high = n - 1; // REVIEW high is n-1
     while (low <= high)
     {
         int mid = (low + high) / 2;
@@ -23,14 +23,5 @@ int missingK(vector<int> vec, int n, int k)
             high = mid - 1;
         }
     }
-    return k + high + 1; //REVIEW  or  low+k
-}
-
-int main()
-{
-    vector<int> vec = {4, 7, 9, 10};
-    int n = 4, k = 4;
-    int ans = missingK(vec, n, k);
-    cout << "The missing number is: " << ans << "\n";
-    return 0;
+    return k + high + 1; // REVIEW  or  low+k
 }
