ae4cd20aae231d2d98050659028d0a6d0d76e587
Priyansh Jain <priyansh01jain@gmail.com>
2025-07-02 10:07:01 +0530

    Updated !

--- Binary_Search/14-Find-The-Peak-Element.cpp
+++ Binary_Search/14-Find-The-Peak-Element.cpp
@@ -16,7 +16,7 @@ int findPeakElement(vector<int> &arr)
     if (arr[n - 1] != arr[n - 2])
         return arr[n - 1];
 
-    int low = 1, high = n - 2; // REVIEW
+    int low = 1, high = n - 2; // REVIEW 
     while (low < high)         // NOTE < not <=
     {
         int mid = (low + high) / 2;
@@ -30,12 +30,4 @@ int findPeakElement(vector<int> &arr)
             low = mid + 1;
     }
     return low; // REVIEW or return high  as per question !
-}
-
-int main()
-{
-    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 5, 1};
-    int ans = findPeakElement(arr);
-    cout << "The peak is at index: " << ans << "\n";
-    return 0;
 }
