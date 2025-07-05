74bc62f8ff715e71822e1f36872af2ada3acac04
Priyansh Jain <priyansh01jain@gmail.com>
2025-07-05 21:02:43 +0530

    Updated !

--- Bit Manupulation/11 Count the number of bit flips required to change A to B .cpp
+++ Bit Manupulation/11 Count the number of bit flips required to change A to B .cpp
@@ -7,7 +7,7 @@ int countBitFlips(int A, int B) // REVIEW -  a is start and b is goal
     int diff = A ^ B; // XOR of A and B gives the positions where bits are different
 
     int count = 0;
-    while (diff > 0)
+    while (diff > 0) 
     {
         diff = diff & (diff - 1); // Removes the rightmost set bit
         count++;
