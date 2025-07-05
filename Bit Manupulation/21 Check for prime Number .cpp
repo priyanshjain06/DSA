58eae8177953c40cf8af5a8a6b53853333812e41
Priyansh Jain <priyansh01jain@gmail.com>
2025-07-05 22:35:30 +0530

    Updated !

--- Bit Manupulation/21 Check for prime Number .cpp
+++ Bit Manupulation/21 Check for prime Number .cpp
@@ -1,15 +1,13 @@
 // REVIEW -  Time Complexity : O(sqrt(n))
+
 #include <iostream>
 #include <math.h>
 using namespace std;
 
 bool checkPrime(int n)
 {
-
     int cnt = 0;
 
-    // Loop through numbers from 1
-    // to the square root of n.
     for (int i = 1; i <= sqrt(n); i++) // REVIEW start from 1
     {
 
@@ -18,7 +16,6 @@ bool checkPrime(int n)
         if (n % i == 0)
         {
 
-            // Increment the counter.
             cnt = cnt + 1;
 
             // If n is not a perfect square,
@@ -33,33 +30,8 @@ bool checkPrime(int n)
     // If the number of
     // factors is exactly 2.
     if (cnt == 2)
-    {
-        // Return true, indicating
-        // that the number is prime.
-        return true;
-    }
-    // If the number of
-    // factors is not 2.
+        return true; // REVIEW means n is prime number
+
     else
-    {
-        // Return false, indicating
-        // that the number is not prime.
         return false;
-    }
-}
-
-int main()
-{
-    int n = 1483;
-    bool isPrime = checkPrime(n);
-    if (isPrime)
-    {
-        cout << n << " is a prime number." << endl;
-    }
-    else
-    {
-        cout << n << " is not a prime number." << endl;
-    }
-
-    return 0;
 }
