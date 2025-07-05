2fe1b54f2d3f174f4f46b00e2fdfae30b2a4be83
Priyansh Jain <priyansh01jain@gmail.com>
2025-07-05 22:31:25 +0530

    Updated !

--- Bit Manupulation/20 Find the xor of all the elements from l to r of an array containing 1 to n elements.cpp
+++ Bit Manupulation/20 Find the xor of all the elements from l to r of an array containing 1 to n elements.cpp
@@ -1,10 +1,9 @@
 // REVIEW Time Complexity : O(1) and Space Complexity : O(1)
-// NOTE see screenshot too 
+// NOTE see screenshot too
 
 #include <iostream>
 using namespace std;
 
-// Function to compute XOR from 1 to n
 int xorTillN(int n)
 {
     // XOR pattern based on n % 4
@@ -16,29 +15,19 @@ int xorTillN(int n)
         return n + 1;
     return 0; // n % 4 == 3
 }
-// Function to compute XOR from l to r
+
 int findXorInRange(int l, int r)
 {
-    return xorTillN(r) ^ xorTillN(l - 1); //REVIEW 
-}
-int main()
-{
-    int l, r;
-    cout << "Enter the range (l and r): ";
-    cin >> l >> r;
-
-    int result = findXorInRange(l, r);
-    cout << "XOR from " << l << " to " << r << " is: " << result << endl;
-    return 0;
+    return xorTillN(r) ^ xorTillN(l - 1); // REVIEW
 }
 
-//n       XOR from 1 to n
-// -------------------------
-// 1       1              (1)
-// 2       1 ^ 2          = 3
-// 3       1 ^ 2 ^ 3      = 0
-// 4       1 ^ 2 ^ 3 ^ 4  = 4
-// 5       1 ^ 2 ^ 3 ^ 4 ^ 5 = 1
-// 6       1 ^ 2 ^ 3 ^ 4 ^ 5 ^ 6 = 7
-// 7       1 ^ 2 ^ 3 ^ 4 ^ 5 ^ 6 ^ 7 = 0
-// 8       1 ^ 2 ^ 3 ^ 4 ^ 5 ^ 6 ^ 7 ^ 8 = 8
+// n       XOR from 1 to n
+//  -------------------------
+//  1       1              (1)
+//  2       1 ^ 2          = 3
+//  3       1 ^ 2 ^ 3      = 0
+//  4       1 ^ 2 ^ 3 ^ 4  = 4
+//  5       1 ^ 2 ^ 3 ^ 4 ^ 5 = 1
+//  6       1 ^ 2 ^ 3 ^ 4 ^ 5 ^ 6 = 7
+//  7       1 ^ 2 ^ 3 ^ 4 ^ 5 ^ 6 ^ 7 = 0
+//  8       1 ^ 2 ^ 3 ^ 4 ^ 5 ^ 6 ^ 7 ^ 8 = 8
