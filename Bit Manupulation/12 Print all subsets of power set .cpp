26135eaf04e45efda1b2e1ff4dcc6399b5ef955a
Priyansh Jain <priyansh01jain@gmail.com>
2025-07-05 21:11:57 +0530

    Updated !

--- Bit Manupulation/12 Print all subsets of power set .cpp
+++ Bit Manupulation/12 Print all subsets of power set .cpp
@@ -6,14 +6,14 @@ using namespace std;
 void printSubsets(const vector<int> &set)
 {
     int n = set.size();
-    int totalSubsets = 1 << n; // 2^n subsets
+    int totalSubsets = 1 << n; // REVIEW  2^n subsets
 
-    for (int mask = 0; mask < totalSubsets; ++mask)
+    for (int mask = 0; mask < totalSubsets; ++mask) // REVIEW
     {
         cout << "{ ";
-        for (int i = 0; i < n; ++i) //REVIEW - 
+        for (int i = 0; i < n; ++i) // REVIEW -
         {
-            if (mask & (1 << i))
+            if (mask & (1 << i)) // REVIEW check if the ith bit is set
             {
                 cout << set[i] << " ";
             }
