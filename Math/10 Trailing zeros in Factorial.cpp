3196d26f7c4be0913d6f75713f2fc15551ec2951
Priyansh Jain <priyansh01jain@gmail.com>
2025-07-14 18:53:25 +0530

    Updated !

--- Math/10 Trailing zeros in Factorial.cpp
+++ Math/10 Trailing zeros in Factorial.cpp
@@ -1,7 +1,14 @@
-int countTrailingZeroes(int n) {
+int countTrailingZeroes(int n)
+{
     int count = 0;
-    for (int i = 5; n / i > 0; i *= 5) {
+    for (int i = 5; n / i > 0; i *= 5)
+    {
         count += n / i;
     }
     return count;
 }
+
+// REVIEW -  dry run
+// Input: n = 11
+// 11 /5 = 2 > 0  , count =2
+// 11 /25  not > 0 ,  so end loop
